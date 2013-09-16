1d15f7a943b7ef1d6ae1e25538c5ea74a0e9fb42
Elfhir <ta.thanhliem@gmail.com>
2013-09-17 00:06:48 +0200

    fix layout of score and preview problems

--- js/jTetris.js
+++ js/jTetris.js
@@ -8,22 +8,47 @@
 var jTetris = {
 
 	// Vars
-	wrapper : null,
-	that : null,
-	infoZone : null,
-	gameZone : null,
-	keysZone : null,
+	wrapper		:	null,
+	that		:	null,
+	infoZone	:	null,
+	gameZone	:	null,
+	keysZone	:	null,
+	tetriminoes	:	null,
+	score		:	null,
 
 	keys : {
-		left: 37,
-		up: 38,
-		right: 39,
-		down: 40,
-		z: 90,
-		q: 81,
-		s: 83,
-		d: 68,
-		space: 32
+		left	:	37,
+		up		:	38,
+		right	:	39,
+		down	:	40,
+		z		:	90,
+		q		:	81,
+		s		:	83,
+		d		:	68,
+		space	:	32
+	},
+
+	stage_width				:	300,
+	stage_height			:	300,
+	score_height			:	200,
+	score_width				:	100,
+	preview_height			:	100,
+	preview_width			:	100,
+	square_size				:	10,
+	stroke_size				:	2,
+
+	colors : {
+		white:		"#ffffff",
+		cream:		"#dddddd",
+		grey:		"#aaaaaa",
+		greyBlack:	"#888888",
+		blackGrey:	"#222222",
+		black:		"#000000",
+		orange:		"#ff400d",
+		cyan:		"#00eeff",
+		blue:		"#0088ff",
+		marine:		"#0000ff",
+		violet:		"#6000ff"
 	},
 
 	// Methods
@@ -34,7 +59,6 @@ var jTetris = {
 		jTetris.keyboardListener();
 		
 		jTetris.setupKinetic();
-		window.console.log(jTetris.that.style.offsetHeight);
 		//jTetris.createTetrominoes();
 	},
 	
@@ -78,33 +102,61 @@ var jTetris = {
 		jTetris.that = document.getElementById("jTetris");
 		jTetris.infoZone = document.getElementById("jTetris-info-zone");
 		jTetris.gameZone = document.getElementById("jTetris-game-zone");
-		jTetris.tetriminoesInfo = document.getElementById("jTetris-tetriminoes-info");
+		jTetris.scoreInfo = document.getElementById("jTetris-score-info");
+		jTetris.preview = document.getElementById("jTetris-tetriminoes-info");
 	},
 
 	setupKinetic : function() {
-		var stage = new Kinetic.Stage({
-			container: jTetris.that,
-			width: 300,
-			height: 300
+		// Game Zone #jTetris
+		var gameStage = new Kinetic.Stage({
+			container: 	jTetris.that,
+			width: 		jTetris.stage_width,
+			height: 	jTetris.stage_height
+		});
+
+		// tetriminoes preview Zone #jTetris-info-zone
+		var previewStage = new Kinetic.Stage({
+			container:	jTetris.preview,
+			width:		jTetris.preview_width,
+			height:		jTetris.preview_height
+		});
+
+		// Score Zone #jTetris-info-zone
+		var scoreStage = new Kinetic.Stage({
+			container:	jTetris.scoreInfo,
+			width:		jTetris.score_width,
+			height:		jTetris.score_height
 		});
 
 		var gameLayer = new Kinetic.Layer();
+		var scoreLayer = new Kinetic.Layer();
+		var previewLayer = new Kinetic.Layer();
 
 		var rect = new Kinetic.Rect({
-	        x: 239,
-	        y: 75,
-	        width: 100,
-	        height: 50,
-	        fill: 'green',
-	        stroke: 'black',
-	        strokeWidth: 4
-	      });
+			x: 0,
+			y: 0,
+			width:			jTetris.square_size,
+			height:			jTetris.square_size,
+			fill:			jTetris.colors.violet,
+			stroke:			jTetris.colors.blackGrey,
+			strokeWidth:	jTetris.stroke_size
+		});
 
-	      // add the shape to the layer
-	      gameLayer.add(rect);
+		var score = new Kinetic.Text({
+			x: scoreStage.getWidth()/6,
+  			y: 15,
+			text:"0000",
+			fontSize: 12,
+			fontFamily: 'Calibri',
+			fill: jTetris.colors.cyan
+		});
 
-	      // add the layer to the stage
-	      stage.add(gameLayer);
+		scoreLayer.add(score);
+		gameLayer.add(rect);
+		
+		gameStage.add(gameLayer);
+		scoreStage.add(scoreLayer);
+		previewStage.add(previewLayer);
 
 	}
 
